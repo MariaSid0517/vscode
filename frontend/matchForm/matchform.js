f5f9108214773dfa8a2560c9209956594fbfdde8
madeeha04 <madeehasiddeeque@gmail.com>
2025-09-24 15:23:59 -0500

    ch

--- frontend/matchForm/matchform.js
+++ frontend/matchForm/matchform.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Auto-filled values (simulated database response)
-    const volunteerData = {
-        name: "Aisha Khan",
-        matchedEvent: "Community Food Drive"
-    };
 
     // Fill input fields directly
     document.getElementById("volunteerName").value = volunteerData.name;
